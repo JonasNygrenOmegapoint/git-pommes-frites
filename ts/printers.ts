d14e53edd3ee78080a6a2cb328f46a433a904df9
Jonas Nygren <jonas.nygren@omegapoint.se>
2026-03-12 10:20:33 +0100

    Print first even number

--- ts/printers.ts
+++ ts/printers.ts
@@ -30,4 +30,5 @@ export const printFirsts = () => {
   console.log("First reverse number", getReverseDataset()[0]);
   console.log("First big number", getBigNumbers()[1]);
   console.log("First small number", getSmallNumbers()[0]);
+  console.log("First even number", getEvenNumbers()[0]);
 };
