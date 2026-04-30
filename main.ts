f1eec72d4dab740afcfdaba630a460bb864500c5
Jonas Nygren <jonas.nygren@omegapoint.se>
2026-04-30 14:43:53 +0200

    Add function to print indifferent message

--- main.ts
+++ main.ts
@@ -11,6 +11,10 @@ const printNiceMessage = () => {
   printMessage("Happy to be of service!");
 };
 
+const printIndifferentMessage = () => {
+  printMessage("I have no string feeling one way or the other.");
+};
+
 const printMessage = (message: string) => {
   console.log(message);
 };
