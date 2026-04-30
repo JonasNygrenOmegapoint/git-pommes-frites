da6331332ef44e734d1189aeea173903badfd6b1
Jonas Nygren <jonas.nygren@omegapoint.se>
2026-04-30 14:40:53 +0200

    Add function to print eerie message

--- main.ts
+++ main.ts
@@ -10,6 +10,10 @@ const printNiceMessage = () => {
   printMessage("Happy to be of service!");
 };
 
+const printEerieMessage = () => {
+  printMessage("I'm sorry Dave, I'm afraid I can't do that.");
+};
+
 const printMessage = (message: string) => {
   console.log(message);
 };
